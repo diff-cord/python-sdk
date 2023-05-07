f1af7b438df12eff1bbbdce0cbbab935da059639
1elasmarjad <42979241+1elasmarjad@users.noreply.github.com>
2023-05-07 16:10:42 -0400

    FIX: since_last_vote can be None

--- diffcord/vote.py
+++ diffcord/vote.py
@@ -49,7 +49,7 @@ class UserVoteInformation:
     """ Represents a user vote information.
     """
 
-    def __init__(self, user_id: str, bot_id: str, monthly_votes: int, since_last_vote: int, until_next_vote: int):
+    def __init__(self, user_id: str, bot_id: str, monthly_votes: int, since_last_vote: int | None, until_next_vote: int):
         self.user_id: str = user_id
         """ The id of the target user. """
 
@@ -59,17 +59,23 @@ class UserVoteInformation:
         self.monthly_votes: int = monthly_votes
         """ The number of votes this user has for the given bot this month. """
 
-        self.since_last_vote: datetime.timedelta = datetime.timedelta(seconds=since_last_vote)
+        self.since_last_vote: datetime.timedelta = datetime.timedelta(seconds=since_last_vote) if since_last_vote is not None else None
         """ Time in seconds since the last vote. """
 
         self.until_next_vote: datetime.timedelta = datetime.timedelta(seconds=until_next_vote)
         """ Time in seconds till the next vote. """
 
-        self.last_vote: datetime.datetime = datetime.datetime.now() - self.since_last_vote
+        self.last_vote: datetime.datetime = datetime.datetime.now() - self.since_last_vote if self.since_last_vote is not None else None
         """ The last time the user voted. """
 
         self.next_vote: datetime.datetime = datetime.datetime.now() + self.until_next_vote
         """ The next time the user can vote. """
+
+    @property
+    def can_vote(self) -> bool:
+        """ Whether the user can vote or not.
+        """
+        return not self.until_next_vote or self.until_next_vote.total_seconds() <= 0
 
     def __repr__(self):
         return f"<UserVoteInformation user_id={self.user_id} bot_id={self.bot_id} votes_this_month={self.monthly_votes} since_last_vote={self.since_last_vote} until_next_vote={self.until_next_vote}>"
