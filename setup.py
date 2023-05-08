3c4bb50366ecd32998bd3b02d02398c3d158ec79
1elasmarjad <42979241+1elasmarjad@users.noreply.github.com>
2023-05-08 16:39:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='diffcord',
-    version='0.0.7',
+    version='0.0.8',
     description="A Python wrapper for the Diffcord API written in Python.",
     packages=find_packages(),
     author='jadelasmar4@gmail.com',
